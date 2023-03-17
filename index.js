e95f56a030bcad0398ddce74c46250253ec68889
David <david.rolin@gmail.com>
2023-03-17 12:59:42 +0100

    update to new API

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const Verisure = require('verisure');
+// const Verisure = require('verisure');
 const mqtt = require('mqtt');
 var asyncInterval = require('asyncinterval');
+const axios = require('axios');
+const VerisureInstallation = require('./installation');
+
+const AUTH_HOSTS = [
+    'm-api01.verisure.com',
+    'm-api02.verisure.com',
+  ];
+  
+  const HOSTS = [
+    'e-api01.verisure.com',
+    'e-api02.verisure.com',
+  ];
+  
 
 // Set to true to output all received info to console.
 // False will limit output to only mention that a query to the Verisure API is made
@@ -37,6 +50,8 @@ const options = {
 	retain: true,
 	}
 }
+
+
 
 if (flagDebug) {
     console.log(`Verisure username: ${config.verisureUsername}`);
@@ -104,6 +119,9 @@ function wait(timeout) {
         }, timeout);
     });
 }
+
+
+
 
 function getVerisure() {
     try {
@@ -193,7 +211,7 @@ function getVerisure() {
 									  "json_attributes_topic":`${config.mqttRootTopic}/${verisure_prefix}/tele/${climateValue.deviceArea}/SENSOR`,
 									  "name":`${climateValue.deviceArea}_temperature`,
 									  "unique_id":`${climateValue.deviceLabel.replace(' ','_')}_2`,
-									  "unit_of_measurement": "°C",
+									  "unit_of_measurement": "ï¿½C",
 									  "device":
 									  {
 										  "identifiers": [
@@ -444,3 +462,119 @@ interval.onTimeout(function () {
     console.log('Timeout!');
     console.log('XXXXXXXXXXXXXXXXXXXXXXXX');
 });
+
+class Verisure {
+    constructor(email, password, cookies = []) {
+      [this.host] = HOSTS;
+      [this.authHost] = AUTH_HOSTS;
+      this.email = email;
+      this.password = password;
+      this.promises = {};
+      this.cookies = cookies;
+    }
+  
+    async makeRequest(options, retrying = false) {
+      const isAuth = options.url.startsWith('/auth');
+  
+      if (retrying) {
+        if (isAuth) {
+          this.authHost = AUTH_HOSTS[+!AUTH_HOSTS.indexOf(this.authHost)];
+        } else {
+          this.host = HOSTS[+!HOSTS.indexOf(this.host)];
+        }
+      }
+  
+      const request = {
+        ...options,
+        baseURL: isAuth
+          ? `https://${this.authHost}/`
+          : `https://${this.host}/xbn/2/`,
+        headers: {
+          'User-Agent': 'node-verisure',
+          accept: 'application/json',
+          ...(options.headers || {}),
+        },
+      };
+  
+      if (this.cookies) {
+        request.headers.Cookie = this.cookies.join(';');
+      }
+  
+      try {
+        return await axios(request);
+      } catch (error) {
+        if (error.response && error.response.status > 499 && !retrying) {
+          return this.makeRequest(options, true);
+        }
+  
+        throw error;
+      }
+    }
+  
+    client(request) {
+      const requestRef = JSON.stringify(request);
+      let promise = this.promises[requestRef];
+      if (promise) {
+        return promise;
+      }
+  
+      promise = this.makeRequest(request)
+        .then(({ data }) => {
+          delete this.promises[requestRef];
+          return data;
+        })
+        .catch((error) => {
+          delete this.promises[requestRef];
+          return Promise.reject(error);
+        });
+  
+      this.promises[requestRef] = promise;
+      return promise;
+    }
+  
+    getCookie(prefix) {
+      return this.cookies.find((cookie) => cookie.startsWith(prefix));
+    }
+  
+    async getToken(code) {
+      let authRequest = {
+        method: 'post',
+        url: '/auth/login',
+        auth: {
+          username: this.email,
+          password: this.password,
+        },
+      };
+  
+      if (code) {
+        // 2. Continue MFA flow, send code.
+        authRequest = {
+          method: 'post',
+          url: '/auth/mfa/validate',
+          data: { token: code },
+        };
+      }
+  
+      const { headers } = await this.makeRequest(authRequest);
+  
+      const cookies = headers['set-cookie'];
+  
+      this.cookies = cookies && cookies.map((cookie) => cookie.split(';')[0]);
+  
+      if (this.getCookie('vs-stepup')) {
+        // 1. Start MFA flow, request code.
+        await this.makeRequest({
+          method: 'post',
+          url: '/auth/mfa',
+        });
+      }
+  
+      return this.cookies;
+    }
+  
+    getInstallations() {
+      return this.client({ url: `/installation/search?email=${this.email}` })
+        .then((installations) => installations
+          .map((installation) => new VerisureInstallation(installation, this.client.bind(this))));
+    }
+  }
---
+++ installation.js
@@ -0,0 +1,22 @@
+class VerisureInstallation {
+    constructor(installation, client) {
+      this.giid = installation.giid;
+      this.locale = installation.locale;
+      this.config = installation;
+  
+      this.baseClient = client;
+    }
+  
+    client(options) {
+      const requestOptions = Object.assign(options, {
+        url: `/installation/${this.giid}/${options.url}`,
+      });
+      return this.baseClient(requestOptions);
+    }
+  
+    getOverview() {
+      return this.client({ url: 'overview' });
+    }
+  }
+  
+  module.exports = VerisureInstallation;
